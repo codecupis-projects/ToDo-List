72aaecb1f7ecb0683baa0e2a5d04676ac489ffcb
Ali Dorri <transcendentimagination@gmail.com>
2025-02-20 13:38:46 +0330

    feat Add test for slots

--- components/sampleComponent/runtime/component.js
+++ components/sampleComponent/runtime/component.js
@@ -3,6 +3,6 @@ import { defineComponent } from '../../../scripts/components.js';
 defineComponent({tagName: "my-component",
     meta: import.meta,
     templatePath: "template.html",
-    stylePaths: ["styles.css"],
+    stylePaths: ["styles.css", "styles.css", "styles.css"],
     scriptPaths: ["myScript.js"]
 });
--- components/sampleComponent/runtime/myScript.js
+++ components/sampleComponent/runtime/myScript.js
@@ -1,3 +1,10 @@
 export function start(shadow){
-    console.log(shadow.querySelectorAll("label")[0].textContent);
+    console.log(`My Script started. Shadow root's child count: ${shadow.children.length}`);
+    someFunction(shadow.querySelectorAll("label")[0]);
+    someFunction(shadow.querySelectorAll("label")[1]);
+    someFunction(shadow.querySelectorAll("label")[2]);
+}
+
+function someFunction(label){
+    console.log(label.textContent);
 }
